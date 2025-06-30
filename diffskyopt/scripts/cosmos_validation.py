bc9945a2aba620467501d2b033dc87099299fee6
Alan Pearl <alanpearl13@gmail.com>
2025-06-30 16:58:16 -0500

    Fit to COSMOS and validation n_of_* plots

---
+++ diffskyopt/scripts/cosmos_validation.py
@@ -0,0 +1,208 @@
+import argparse
+from mpi4py import MPI
+
+if not MPI.COMM_WORLD.rank:
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+
+import numpy as np
+import jax
+# import jax.numpy as jnp
+from astropy.cosmology import Planck15
+from astropy import units
+
+from ..lossfuncs.cosmos_fit import COSMOS_SKY_AREA, CosmosFit
+# from .. import trange
+
+ran_key = jax.random.key(1)
+
+
+def cosmos_volume(z_min, z_max):
+    """Volume in spherical shell between z_min < z < z_max in (Mpc/h)^3"""
+    full_sky_area = (4 * np.pi) * (180 / np.pi) ** 2  # square degrees
+    fsky = COSMOS_SKY_AREA / full_sky_area
+    vol_com_sphere = Planck15.comoving_volume(  # type: ignore
+        z_max) - Planck15.comoving_volume(z_min)  # type: ignore
+    return fsky*vol_com_sphere / (units.Mpc / Planck15.h)**3  # type: ignore
+
+
+def n_of_z_plot(cosmos_fit, save=False, zbins=30,
+                ax=None, show=True, make_legend=True,
+                model_params=None, prefix="", ithresh=25.0,
+                data_label="COSMOS", model_label="Diffsky Model"):
+    if model_params is None:
+        model_params = cosmos_fit.default_u_param_arr
+    # Plot a color-magnitude diagram: g-r vs i-band
+    data_targets, data_weights = (cosmos_fit.data_targets,
+                                  cosmos_fit.data_weights)
+
+    # Compute model targets + weights with default u_params
+    # and overplot with a seaborn.kdeplot
+    model_targets, model_weights = \
+        cosmos_fit.targets_and_weights_from_params(
+            model_params, ran_key, modelsamp=False)
+    model_targets = np.concatenate(MPI.COMM_WORLD.allgather(model_targets))
+    model_weights = np.concatenate(MPI.COMM_WORLD.allgather(model_weights))
+    if not MPI.COMM_WORLD.rank:
+        data_z = data_targets[:, -1]
+        model_z = model_targets[:, -1]
+        all_z = np.concatenate([data_z, model_z])
+        z_edges = np.linspace(all_z.min(), all_z.max(), zbins)
+        z_cens = 0.5 * (z_edges[:-1] + z_edges[1:])
+        data_n_of_z = np.histogram(data_z, z_edges, weights=data_weights)[0]
+        model_n_of_z = np.histogram(model_z, z_edges, weights=model_weights)[0]
+
+        # cosmos_volume_per_zbin = np.array([
+        #     cosmos_volume(*z_edges[i:i+2]) for i in range(len(z_cens))])
+        # data_n_of_z /= cosmos_volume_per_zbin
+        # model_n_of_z /= cosmos_volume_per_zbin
+        dz = np.diff(z_edges)
+        data_n_of_z /= dz
+        model_n_of_z /= dz
+
+        if ax is None:
+            ax = plt.subplots()[1]
+        ax.plot(z_cens, data_n_of_z, label=data_label)
+        ax.plot(z_cens, model_n_of_z, label=model_label)
+        ax.semilogy()
+        ax.set_xlabel("$z$")
+        ax.set_ylabel(f"$\\rm n(z | m_i < {ithresh:.1f})$")
+
+        if make_legend:
+            ax.legend(frameon=False)
+        if save:
+            plt.savefig(f"{prefix}n_of_z.png", bbox_inches="tight")
+            plt.clf()
+        elif show:
+            plt.show()
+            plt.clf()
+
+
+def n_of_ithresh_plot(cosmos_fit, save=False, ibins=30, ithresh=25.0,
+                      ax=None, show=True, make_legend=True,
+                      model_params=None, prefix="",
+                      data_label="COSMOS", model_label="Diffsky Model"):
+    if model_params is None:
+        model_params = cosmos_fit.default_u_param_arr
+    data_targets, data_weights = (cosmos_fit.data_targets,
+                                  cosmos_fit.data_weights)
+
+    model_targets, model_weights = \
+        cosmos_fit.targets_and_weights_from_params(
+            model_params, ran_key, modelsamp=False)
+    model_targets = np.concatenate(MPI.COMM_WORLD.allgather(model_targets))
+    model_weights = np.concatenate(MPI.COMM_WORLD.allgather(model_weights))
+    if not MPI.COMM_WORLD.rank:
+        data_i = data_targets[:, 0]
+        model_i = model_targets[:, 0]
+        all_i = np.concatenate([data_i, model_i])
+        i_vals = np.linspace(all_i.min(), ithresh, ibins)
+        data_hist = np.cumsum(
+            np.histogram(data_i, i_vals, weights=data_weights)[0])
+        model_hist = np.cumsum(
+            np.histogram(model_i, i_vals, weights=model_weights)[0])
+
+        ess = np.sum(data_weights)**2 / np.sum(data_weights**2)
+        f_ess = ess / np.sum(data_weights)
+        data_n_of_ithresh = data_hist / COSMOS_SKY_AREA
+        model_n_of_ithresh = model_hist / COSMOS_SKY_AREA
+        frac_err = 1 / np.sqrt(f_ess * data_hist) + 0.1
+        data_err = data_n_of_ithresh * frac_err
+
+        # Fractional residuals
+        frac_resid = (model_n_of_ithresh - data_n_of_ithresh
+                      ) / data_n_of_ithresh
+
+        if ax is None:
+            fig = plt.figure(figsize=(6, 6))
+            gs = GridSpec(2, 1, height_ratios=[2, 1], hspace=0)
+            ax = fig.add_subplot(gs[0])
+            ax_resid = fig.add_subplot(gs[1], sharex=ax)
+        else:
+            fig = ax.figure
+            gs = None
+            ax_resid = None  # Not supported if ax is provided
+
+        # Upper panel: cumulative counts
+        ax.plot(i_vals[1:], data_n_of_ithresh, label=data_label, color="C0")
+        ax.fill_between(i_vals[1:], data_n_of_ithresh + data_err,
+                        data_n_of_ithresh - data_err, color="C0", alpha=0.5)
+        ax.plot(i_vals[1:], model_n_of_ithresh, label=model_label, color="C1")
+        ax.semilogy()
+        ax.set_ylabel("$\\rm n(<mag) \\; [deg^{-2}]$")
+        if make_legend:
+            ax.legend(frameon=False)
+        ax.tick_params(labelbottom=False)  # Hide x labels on upper panel
+
+        # Lower panel: fractional residuals
+        if ax_resid is not None:
+            ax_resid.axhline(0, color="C0")
+            ax_resid.fill_between(i_vals[1:], -frac_err, frac_err,
+                                  color="C0", alpha=0.5)
+            ax_resid.plot(i_vals[1:], frac_resid, color="C1")
+            ax_resid.set_ylabel("$\\rm \\Delta n / n$")
+            ax_resid.set_xlabel("$\\rm m_i$")
+            ax_resid.set_ylim(-1, 1)
+
+            # Remove the top y-tick label to avoid overlap
+            yticks = ax_resid.get_yticks()
+            yticklabels = [f"{x:g}" for x in yticks]
+            if len(yticklabels) > 1:
+                yticklabels[-1] = ""
+            ax_resid.set_yticklabels(yticklabels)
+            # Remove space between panels
+            plt.subplots_adjust(hspace=0)
+            plt.setp(ax.get_xticklabels(), visible=False)
+
+        if save:
+            plt.savefig(f"{prefix}n_of_ithresh.png", bbox_inches="tight")
+            plt.clf()
+        elif show:
+            plt.show()
+            plt.clf()
+
+
+parser = argparse.ArgumentParser(
+    description="Validate model vs COSMOS data with n(z) and n(<m_i)")
+parser.add_argument(
+    "-s", "--save", action="store_true",
+    help="Save plots instead of opening interactive window")
+parser.add_argument(
+    "--prefix", type=str, default="",
+    help="Prefix for saved plot filenames, ignored if --save is not set")
+parser.add_argument(
+    "--iband-max", type=float, default=25.0,
+    help="Only fit to data with apparent mag_i < IBAND_MAX")
+parser.add_argument(
+    "-m", "--lgmp-min", type=float, default=10.5,
+    help="Minimum lgmp value for mc lightcone")
+parser.add_argument(
+    "-a", "--sky-area-degsq", type=float, default=0.1,
+    help="Sky area in square degrees for the mc lightcone")
+parser.add_argument(
+    "--param-results", type=str, default=None,
+    help="Results .npz file to load final parameter value from")
+parser.add_argument(
+    "--hmf-calibration", type=str, default=None,
+    help="Specify diffmahpop params ('smdpl_hmf', 'smdpl_shmf', 'hacc_shmf')")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    cosmos_fit = CosmosFit(
+        i_thresh=args.iband_max,
+        lgmp_min=args.lgmp_min,
+        sky_area_degsq=args.sky_area_degsq,
+        hmf_calibration=args.hmf_calibration)
+
+    model_params = None
+    if args.param_results:
+        results = np.load(args.param_results)
+        if "best_params" in results.keys():
+            model_params = results["best_params"]
+        else:
+            model_params = results["params"][-1]
+
+    n_of_z_plot(cosmos_fit, save=args.save, prefix=args.prefix,
+                ithresh=args.iband_max)
+    n_of_ithresh_plot(cosmos_fit, save=args.save, prefix=args.prefix,
+                      ithresh=args.iband_max)
---
+++ diffskyopt/scripts/kdescent_fit_to_cosmos.py
@@ -0,0 +1,93 @@
+import argparse
+from mpi4py import MPI
+
+import numpy as np
+import jax
+
+from ..lossfuncs.cosmos_fit import CosmosFit
+
+
+def adam_fit(cosmos_fit, out, seed=1, nsteps=1000, u_param_init=None,
+             learning_rate=0.1, progress=True, modelsamp=True):
+    # Run Adam optimization from random initializations and plot loss
+    calc = cosmos_fit.get_multi_grad_calc(modelsamp=modelsamp)
+    key = jax.random.key(seed)
+    if u_param_init is None:
+        u_param_init = cosmos_fit.default_u_param_arr
+
+    params, losses = calc.run_adam(
+        u_param_init, nsteps=nsteps, learning_rate=learning_rate,
+        randkey=key, progress=progress)
+
+    if not MPI.COMM_WORLD.rank:
+        np.savez(
+            out,
+            params=np.asarray(params),
+            losses=np.asarray(losses)
+        )
+
+
+parser = argparse.ArgumentParser(
+    description="Test real fits to COSMOS data")
+parser.add_argument(
+    "-o", "--output", type=str, default="cosmos_fit_results.npz",
+    help="Save plots instead of opening interactive window")
+parser.add_argument(
+    "-i", "--input", type=str, default=None,
+    help="Previous results of which to start from the final params")
+parser.add_argument(
+    "-n", "--nsteps", type=int, default=1000,
+    help="Number of adam fitting steps")
+parser.add_argument(
+    "-l", "--learning-rate", type=float, default=0.1,
+    help="Learning rate for adam gradient descent")
+parser.add_argument(
+    "-s", "--seed", type=int, default=1,
+    help="Random seed")
+parser.add_argument(
+    "--iband-max", type=float, default=25.0,
+    help="Only fit to data with apparent mag_i < IBAND_MAX")
+parser.add_argument(
+    "-m", "--lgmp-min", type=float, default=10.5,
+    help="Minimum lgmp value for mc lightcone")
+parser.add_argument(
+    "-a", "--sky-area-degsq", type=float, default=0.1,
+    help="Sky area in square degrees for the mc lightcone")
+parser.add_argument(
+    "-k", "--num-kernels", type=int, default=40,
+    help="Number of kernels for kdescent")
+parser.add_argument(
+    "-f", "--num-fourier-positions", type=int, default=20,
+    help="Number of Fourier evaluation positions for kdescent")
+parser.add_argument(
+    "--hmf-calibration", type=str, default=None,
+    help="Specify diffmahpop params ('smdpl_hmf', 'smdpl_shmf', 'hacc_shmf')")
+parser.add_argument(
+    "--log-loss", action="store_true",
+    help="Perform logarithm on KDE counts for loss computation")
+parser.add_argument(
+    "--max-n-halos-per-bin", type=int, default=200,
+    help="Number of halos to select from each halo_upweight bin")
+parser.add_argument(
+    "--n-halo-weight-bins", type=int, default=50,
+    help="Number of Fourier evaluation positions for kdescent")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    cosmos_fit = CosmosFit(
+        i_thresh=args.iband_max,
+        lgmp_min=args.lgmp_min,
+        sky_area_degsq=args.sky_area_degsq,
+        num_kernels=args.num_kernels,
+        num_fourier_positions=args.num_fourier_positions,
+        hmf_calibration=args.hmf_calibration,
+        log_loss=args.log_loss,
+        max_n_halos_per_bin=args.max_n_halos_per_bin,
+        n_halo_weight_bins=args.n_halo_weight_bins)
+
+    u_param_init = None
+    if args.input is not None:
+        u_param_init = np.load(args.input)["params"][-1]
+
+    adam_fit(cosmos_fit, args.output, nsteps=args.nsteps,
+             learning_rate=args.learning_rate, u_param_init=u_param_init)
