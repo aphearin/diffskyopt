05d43aa4c4e263176c7adf03625ded4eedf7d56b
Alan Pearl <alanpearl13@gmail.com>
2025-07-02 16:02:14 -0500

    Initial attempt of colors_through_redshift.gif

--- diffskyopt/scripts/colors_through_redshift_gif.py
+++ diffskyopt/scripts/colors_through_redshift_gif.py
@@ -18,7 +18,7 @@ ran_key = jax.random.key(1)
 
 def colors_gif(
     cosmos_fit, model_params, prefix="",
-    num_z_bins=10, bins=30, duration=0.5
+    num_z_bins=20, bins=30, duration=0.5
 ):
 
     if model_params is None:
@@ -51,13 +51,18 @@ def colors_gif(
     z_bins = np.linspace(cosmos_fit.zmin, cosmos_fit.zmax, num_z_bins + 1)
     labels = [*TARGET_LABELS[:1], *TARGET_LABELS[2:]]
 
-    fig, axes = plt.subplots(1, 7, figsize=(28, 4), sharey=False)
+    fig, axes = plt.subplots(3, 3, figsize=(13, 12), sharey=False)
+    axes = axes.ravel()
 
     def plot_slice(i):
         zlo, zhi = z_bins[i], z_bins[i+1]
         data_mask = (z_data >= zlo) & (z_data < zhi)
         model_mask = (z_model >= zlo) & (z_model < zhi)
         for j, ax in enumerate(axes):
+            if j > data_targets.shape[1] - 2:
+                ax.set_visible(False)
+                continue
+
             xidx = j
             yidx = j + 1
             ax.clear()
@@ -65,15 +70,15 @@ def colors_gif(
             H_data, xedges, yedges = np.histogram2d(
                 data_targets[data_mask, xidx], data_targets[data_mask, yidx],
                 bins=bins, weights=data_weights[data_mask])
-            level_max = H_data.max() / 5.0
-            levels = [level_max, level_max / 10.0, level_max / 100.0]
+            level_max = H_data.max() / 2.0
+            levels = level_max / np.array([50.0, 7.0, 1.0])
             X, Y = np.meshgrid(
                 0.5 * (xedges[:-1] + xedges[1:]),
                 0.5 * (yedges[:-1] + yedges[1:]))
             if np.any(H_data > 0):
                 ax.contour(
                     X, Y, H_data.T, levels=levels, colors="C0",
-                    linewidths=1.2, alpha=0.7, label="COSMOS")
+                    linewidths=2, alpha=0.9, label="COSMOS")
             # 2D histogram for model
             H_model, _, _ = np.histogram2d(
                 model_targets[model_mask,
@@ -82,14 +87,15 @@ def colors_gif(
             if np.any(H_model > 0):
                 ax.contour(
                     X, Y, H_model.T, levels=levels, colors="C1",
-                    linewidths=1.2, alpha=0.7, label="Model")
+                    linewidths=2, alpha=0.9, label="Model")
             ax.set_xlabel(labels[xidx])
             ax.set_ylabel(labels[yidx])
             # Dummy handles for legend
             ax.plot([], [], color="C0", label="COSMOS")
-            ax.plot([], [], color="C1", label="Model")
-            ax.legend(frameon=False)
-        fig.suptitle(f"$\\rm {zlo:.2f} < z < {zhi:.2f}$")
+            ax.plot([], [], color="C1", label="Diffsky")
+            if j == 4:
+                ax.legend(frameon=False)
+        fig.suptitle(f"\n$\\rm z = {(zlo+zhi)/2:.2f}$", fontsize=18)
 
     pbar = trange(num_z_bins + 1)
 
@@ -102,7 +108,8 @@ def colors_gif(
         fig, update, frames=num_z_bins, blit=False, repeat=True)
 
     ani.save(f"{prefix}colors-through-redshift.gif",
-             writer='pillow', fps=int(1/duration))
+             writer='pillow', fps=int(1/duration),
+             savefig_kwargs=dict(bbox_inches="tight"))
     plt.close(fig)
 
 
