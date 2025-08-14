7d9773868c5c1941fd7f6c0d8d19e6562f6d7537
Alan Pearl <alanpearl13@gmail.com>
2025-08-14 04:55:11 -0500

    Implement Sobol sequence halo generator

--- diffskyopt/diffsky_model.py
+++ diffskyopt/diffsky_model.py
@@ -222,25 +222,128 @@ def downsample_upweight_lc_data(lc_data, lgmp_min, n_halo_weight_bins=10,
     return downsampled_lc_data, downsampled_halo_weights
 
 
-def generate_weighted_grid_lc_data_kern(
+def get_nhalo_from_grid_interp(tot_num_halos, z_obs, logmp_obs_mf,
+                               z_min, z_max, lgmp_min, lgmp_max,
+                               sky_area_degsq, hmf_params, cosmo_params):
+    ngrid_z = 200
+    ngrid_m = 200
+    ngrid_tot = ngrid_z * ngrid_m
+    z_grid = jnp.linspace(z_min, z_max, ngrid_z)
+    lgmp_grid = jnp.linspace(lgmp_min, lgmp_max, ngrid_m)
+    nhalo_grid = get_nhalo_weighted_lc_grid(
+        lgmp_grid, z_grid, sky_area_degsq, hmf_params, cosmo_params)
+
+    interpolator = jax.scipy.interpolate.RegularGridInterpolator(
+        (z_grid, lgmp_grid), nhalo_grid,
+        bounds_error=False, fill_value=None)  # type: ignore
+
+    interp = interpolator(jnp.column_stack([z_obs, logmp_obs_mf]))
+    return interp * ngrid_tot / tot_num_halos
+
+
+def get_weighted_lightcone_sobol_host_halo_diffmah(
     ran_key,
-    lgmp_min,
-    lgmp_max,
-    num_m_grid,
+    tot_num_halos,
+    z_obs,
+    logmp_obs_mf,
     z_min,
     z_max,
-    num_z_grid,
+    lgmp_min,
+    lgmp_max,
     sky_area_degsq,
-    ssp_data,
-    cosmo_params,
-    tcurves,
-    z_phot_table,
+    cosmo_params=flat_wcdm.PLANCK15,
+    hmf_params=mc_hosts.DEFAULT_HMF_PARAMS,
+    diffmahpop_params=DEFAULT_DIFFMAHPOP_PARAMS,
     logmp_cutoff=0.0,
-    hmf_calibration=None,
 ):
-    lgmp_grid = jnp.linspace(lgmp_min, lgmp_max, num_m_grid)
-    z_grid = jnp.linspace(z_min, z_max, num_z_grid)
-    mclh_args = (ran_key, lgmp_grid, z_grid, sky_area_degsq)
+    """
+    Compute the number of halos on the input halo mass and redshift points
+    """
+
+    nhalo_weights = get_nhalo_from_grid_interp(
+        tot_num_halos, z_obs, logmp_obs_mf,
+        z_min, z_max, lgmp_min, lgmp_max,
+        sky_area_degsq, hmf_params=hmf_params, cosmo_params=cosmo_params,
+    )
+    # nhalo_weights = nhalo_weighted_lc_grid.flatten()
+    # z_obs = np.repeat(z_grid, lgmp_grid.size)
+    # logmp_obs_mf = np.tile(lgmp_grid, z_grid.size)
+
+    t_obs = flat_wcdm.age_at_z(z_obs, *cosmo_params)
+    t_0 = flat_wcdm.age_at_z0(*cosmo_params)
+    lgt0 = jnp.log10(t_0)
+
+    logmp_obs_mf_clipped = np.clip(logmp_obs_mf, logmp_cutoff, np.inf)
+
+    tarr = np.array((10**lgt0,))
+    args = (diffmahpop_params, tarr, logmp_obs_mf_clipped,
+            t_obs, ran_key, lgt0)
+    mah_params_uncorrected = mc_cenpop(*args)[0]  # mah_params, dmhdt, log_mah
+
+    logmp_obs_orig = _log_mah_kern(mah_params_uncorrected, t_obs, lgt0)
+    delta_logmh_clip = logmp_obs_orig - logmp_obs_mf
+    mah_params = mah_params_uncorrected._replace(
+        logm0=mah_params_uncorrected.logm0 - delta_logmh_clip
+    )
+
+    logmp0 = _log_mah_kern(mah_params, 10**lgt0, lgt0)
+    logmp_obs = _log_mah_kern(mah_params, t_obs, lgt0)
+
+    fields = ("z_obs", "t_obs", "logmp_obs", "mah_params", "logmp0")
+    values = (z_obs, t_obs, logmp_obs, mah_params, logmp0)
+    cenpop_out = dict()
+    for key, value in zip(fields, values):
+        cenpop_out[key] = value
+    cenpop_out["nhalos"] = nhalo_weights
+
+    return cenpop_out
+
+
+def generate_weighted_sobol_lc_data(num_halos, z_min, z_max,
+                                    lgmp_min, lgmp_max,
+                                    sky_area_degsq, ran_key=None,
+                                    n_z_phot_table=15, logmp_cutoff=0.0,
+                                    hmf_calibration=None, comm=None):
+    if comm is None:
+        comm = MPI.COMM_WORLD
+    if ran_key is None:
+        ran_key = jax.random.key(0)
+    ran_key, ran_key_sobol = jax.random.split(ran_key, 2)
+
+    # ONLY generate the halos necessary on this rank
+    num_halos_on_rank = num_halos // comm.size + \
+        (1 if comm.rank < num_halos % comm.size else 0)
+    starting_index = comm.rank * \
+        (num_halos // comm.size) + min(comm.rank, num_halos % comm.size)
+
+    # Generate Sobol sequence for halo masses and redshifts
+    seed = int(jax.random.randint(ran_key_sobol, (), 0, 2**31 - 1))
+    bits = None
+    if num_halos > 1e9:
+        # 64-bit sequence required to generate over 2^30 halos
+        bits = 64
+    sampler = qmc.Sobol(d=2, scramble=True, rng=seed, bits=bits)
+    if starting_index > 0:
+        sampler.fast_forward(starting_index)
+
+    with warnings.catch_warnings():
+        # Ignore warning about Sobol sequences not being fully balanced
+        warnings.filterwarnings(
+            "ignore", category=UserWarning, module="scipy.stats")
+        sample = sampler.random(num_halos_on_rank)
+        z_obs, logmp_obs_mf = qmc.scale(
+            sample, (z_min, lgmp_min), (z_max, lgmp_max)).T
+
+    ssp_data = load_ssp_templates(DATA_DIR / SSP_FILE)
+
+    ffiles = [FILTERS_DIR / fn for fn in FILTER_FILES]
+    tcurves = [load_transmission_curve(fn) if str(fn).endswith(".h5") else
+               TransmissionCurve(*np.loadtxt(fn).T) for fn in ffiles]
+
+    z_phot_table = np.linspace(z_min, z_max, n_z_phot_table)
+
+    mclh_args = (ran_key, num_halos, z_obs, logmp_obs_mf, z_min, z_max,
+                 lgmp_min, lgmp_max, sky_area_degsq)
     mclh_kwargs = dict()
     if hmf_calibration == "smdpl_hmf":
         mclh_kwargs["hmf_params"] = smdpl_hmf.HMF_PARAMS
@@ -251,20 +354,21 @@ def generate_weighted_grid_lc_data_kern(
     else:
         assert hmf_calibration is None, f"Unrecognized {hmf_calibration=}"
 
-    res = lc_phot_kern.mclh.get_weighted_lightcone_grid_host_halo_diffmah(
+    res = get_weighted_lightcone_sobol_host_halo_diffmah(
         *mclh_args, logmp_cutoff=logmp_cutoff, **mclh_kwargs)  # type: ignore
 
-    t0 = lc_phot_kern.flat_wcdm.age_at_z0(*cosmo_params)
+    t0 = lc_phot_kern.flat_wcdm.age_at_z0(*DEFAULT_COSMOLOGY)
     t_table = jnp.linspace(lc_phot_kern.T_TABLE_MIN,
                            t0, lc_phot_kern.N_SFH_TABLE)
 
     precomputed_ssp_mag_table = \
-        lc_phot_kern.mclh.get_precompute_ssp_mag_redshift_table(
+        mclh.get_precompute_ssp_mag_redshift_table(
             tcurves, ssp_data, z_phot_table
         )
     wave_eff_table = lc_phot_kern.get_wave_eff_table(z_phot_table, tcurves)
 
     lc_data = lc_phot_kern.LCData(
+        res["nhalos"],
         res["z_obs"],
         res["t_obs"],
         res["mah_params"],
@@ -275,41 +379,7 @@ def generate_weighted_grid_lc_data_kern(
         z_phot_table,
         wave_eff_table,
     )
-    return lc_data, res["nhalos"]
-
-
-def generate_weighted_grid_lc_data(z_min, z_max, num_z_grid,
-                                   lgmp_min, lgmp_max, num_m_grid,
-                                   sky_area_degsq, ran_key=None,
-                                   n_z_phot_table=15, logmp_cutoff=0.0,
-                                   hmf_calibration=None):
-    if ran_key is None:
-        ran_key = jax.random.key(0)
-
-    ssp_data = load_ssp_templates(DATA_DIR / SSP_FILE)
-
-    tcurves = [load_transmission_curve(fn) if str(fn).endswith(".h5") else
-               TransmissionCurve(*np.loadtxt(fn).T) for fn in FILTER_FILES]
-
-    z_phot_table = np.linspace(z_min, z_max, n_z_phot_table)
-
-    lc_data, halo_weights = generate_weighted_grid_lc_data_kern(
-        ran_key,
-        lgmp_min,
-        lgmp_max,
-        num_m_grid,
-        z_min,
-        z_max,
-        num_z_grid,
-        sky_area_degsq,
-        ssp_data,
-        DEFAULT_COSMOLOGY,
-        tcurves,
-        z_phot_table,
-        logmp_cutoff=logmp_cutoff,
-        hmf_calibration=hmf_calibration,
-    )
-    return lc_data, halo_weights
+    return lc_data
 
 
 @jax.jit
